16cbe8d76be75675ab5349fe6c0e66b6543f1733
Daan Pol <daanpol@tutamail.com>
2019-08-27 15:52:05 +0200

    Added AppendingObjectOutputStream

---
+++ src/AppendingObjectOutputStream.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class AppendingObjectOutputStream extends ObjectOutputStream {
+
+    public AppendingObjectOutputStream(OutputStream out) throws IOException {
+        super(out);
+    }
+
+    @Override
+    protected void writeStreamHeader() throws IOException {
+        reset();
+    }
+
+}
