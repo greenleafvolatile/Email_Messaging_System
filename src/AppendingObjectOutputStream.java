a00c5d236284e796d952b4b32f2fdce2cfc829ff
Daan Pol <daanpol@tutamail.com>
2019-08-28 12:01:42 +0200

    Added AppendingObjectOutputStream

--- src/AppendingObjectOutputStream.java
+++ src/AppendingObjectOutputStream.java
@@ -1,5 +1,19 @@
 import java.io.*;
 
+/**
+ * I had a problem:
+ * 1. I'd write multiple User objects to file.
+ * 2. I'd attempt to read the User objects from the file.
+ * 3. Only the User object that was written first would be successfully read, then I'd get a StreamCorruptedException.
+ *
+ * Apparently a file is expected to only have one serialization stream header (written by the ObjectOutputStream object).
+ * Because I was writing multiple User object and creating a new ObjectOutputStream object each time, multiple headers would
+ * be written which caused the StreamCorruptedException.
+ *
+ * AppendingObjectOutputStream, which extends ObjectOutputStream, is the solution. I found it here:
+ * https://stackoverflow.com/questions/1194656/appending-to-an-objectoutputstream
+ */
+
 public class AppendingObjectOutputStream extends ObjectOutputStream {
 
     public AppendingObjectOutputStream(OutputStream out) throws IOException {
