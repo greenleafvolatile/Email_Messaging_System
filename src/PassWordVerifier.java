3e301b6e6a82d8fe289784c953163632e6b8822e
Daan Pol <daanpol@tutamail.com>
2019-09-01 17:26:40 +0200

    Added PasswordVerifier

--- src/PassWordVerifier.java
+++ src/PassWordVerifier.java
@@ -2,31 +2,14 @@ import java.util.logging.Logger;
 
 public class PassWordVerifier {
 
-    public boolean isCorrectPassword(char[] password){
+    public static boolean isCorrectPassword(char[] aPassword){
 
-        boolean hasUpperCase=false;
-        boolean hasLowerCase=false;
-
-        if(password.length!=4){
-            Logger.getGlobal().info("Length: " + password.length);
-            return false;
-        }
-        for(int i=0;i<password.length;i++){
-            if(Character.isUpperCase(password[i])){
-                hasUpperCase=true;
-                Logger.getGlobal().info("Has upper case!");
-
-            }
-            else if(Character.isLowerCase(password[i])){
-                hasLowerCase=true;
-                Logger.getGlobal().info("Has lower case!");
-            }
-            if(hasUpperCase && hasLowerCase){
-                return true;
-            }
-
+        String password=new String(aPassword);
+        boolean hasUpperCase=!password.equals(password.toLowerCase());
+        boolean hasLowerCase=!password.equals(password.toUpperCase());
+        if(hasUpperCase && hasLowerCase && password.length()==4){
+            return true;
         }
         return false;
     }
-
 }
