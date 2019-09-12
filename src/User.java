186409473e6e5dcc832813db9bda69e82815c43c
Daan Pol <daanpol@tutamail.com>
2019-09-12 18:56:55 +0200

    Added User

--- src/User.java
+++ src/User.java
@@ -1,12 +1,12 @@
-import java.util.ArrayList;
+import java.util.*;
 import java.io.Serializable;
 import java.util.logging.Logger;
 
 public class User implements Serializable {
 
-    private String firstName, LastName, username;
+    private String firstName, lastName, username;
     private char[] password;
-    private ArrayList<Message> messages;
+    private List<Message> messages;
 
     public User(String firstName, String lastName, String aUsername, char[] aPassword){
         messages=new ArrayList<>();
@@ -27,11 +27,48 @@ public class User implements Serializable {
         messages.add(message);
     }
 
-    public ArrayList<Message> getMessages(){
+    public List<Message> getMessages(){
         return messages;
     }
 
     public String toString(){
         return new StringBuffer("Username: ").append(username).append(" Password: ").append(password).toString();
+    }
+
+    public int getNrOfMessages(){
+        return messages.size();
+    }
+
+    public void removeMessage(int index){
+        messages.remove(index);
+    }
+
+    @Override
+    public int hashCode(){
+        final int HASH_MULTIPLIER=31;
+        int result=17; // should always return a prime;
+        result=HASH_MULTIPLIER * result + (firstName==null? 0 : firstName.hashCode());
+        result=HASH_MULTIPLIER * result + (lastName==null? 0 : lastName.hashCode());
+        result=HASH_MULTIPLIER * result + (username==null? 0 : username.hashCode());
+        result=HASH_MULTIPLIER * result + (password==null? 0 : Arrays.hashCode(password));
+        result=HASH_MULTIPLIER * result + (messages==null? 0 : messages.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o==this){
+            return true;
+        }
+
+        if(!(o instanceof User)){
+            return false;
+        }
+
+        User comparate=(User) o;
+
+        return this.firstName.compareTo(comparate.firstName)==0 && this.lastName.compareTo(comparate.lastName)==0 && this.username.compareTo(comparate.username)==0 && Arrays.equals(this.password, comparate.password) && this.messages.size()==comparate.messages.size();
+
+
     }
 }
