0fe4034daa24c6b1f6fce84925e3c9a21f2ed770
Daan Pol <daanpol@tutamail.com>
2019-09-19 18:30:51 +0200

    Added NewMessagePane

--- src/NewMessagePane.java
+++ src/NewMessagePane.java
@@ -1,11 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.logging.Logger;
 
-public class NewMessagePane {
-
+class NewMessagePane {
 
     private final User user;
     private JTextArea messageArea;
@@ -13,19 +9,14 @@ public class NewMessagePane {
 
     public NewMessagePane(User thisUser){
         this.user=thisUser;
-
-
         JOptionPane.showOptionDialog(null, createMainPanel(), "New Message", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,  createButtonsArray(), createButtonsArray()[0]);
     }
 
     private JPanel createMainPanel(){
-
         JPanel mainPanel=new JPanel(new BorderLayout(10, 10));
-
         mainPanel.add(createLabelsPanel(), BorderLayout.WEST);
         mainPanel.add(createTextFieldsPanel(), BorderLayout.CENTER);
         mainPanel.add(createMessageArea(), BorderLayout.SOUTH);
-
         return mainPanel;
     }
 
@@ -38,14 +29,15 @@ public class NewMessagePane {
 
         labelsPanel.add(toLabel);
         labelsPanel.add(subjectLabel);
-
         return labelsPanel;
     }
 
     private JPanel createTextFieldsPanel(){
         JPanel textFieldsPanel=new JPanel(new GridLayout(0, 1, 10, 10));
+
         toField=new JTextField();
         subjectField=new JTextField();
+
         textFieldsPanel.add(toField);
         textFieldsPanel.add(subjectField);
         return textFieldsPanel;
@@ -56,50 +48,40 @@ public class NewMessagePane {
         messageArea.setLineWrap(true);
         messageArea.setWrapStyleWord(true);
 
-        JScrollPane scrollPane=new JScrollPane(messageArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-        return scrollPane;
+        return new JScrollPane(messageArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     }
 
     private JButton[] createButtonsArray(){
 
         JButton sendButton=new JButton("Send");
-        sendButton.addActionListener(new ActionListener(){
+        sendButton.addActionListener(event ->{
 
-            public void actionPerformed(ActionEvent event){
+            User recipient=UserController.getUser(toField.getText());
 
-                User recipient=UserController.getUser(toField.getText());
-                Logger.getGlobal().info("messages size before: " + recipient.getNrOfMessages());
-                if(recipient!=null){
+            if(recipient!=null){
 
-                    Message message=new Message(UserController.getUsername(user), toField.getText(), subjectField.getText(), messageArea.getText());
-                    // Add new message to User object.
-                    UserController.addMessage(recipient, message);
-                    // Delete old User object from file.
-                    UserController.removeUserFromFile(recipient);
-                    // Write updated User object to file.
-                    UserController.writeUserToFile(recipient); // writes updated User object to file.
-                    //JOptionPane.getRootFrame().setVisible(false);
-                    JOptionPane.getRootFrame().dispose();
-                    Logger.getGlobal().info("Messages size after sent: " + UserController.getMessages(recipient).size());
-                    //Logger.getGlobal().info("Sent!");
-
-                }
-                else{
-                    Logger.getGlobal().info("User does not exist");
-                    Message message = new Message(UserController.getUsername(user), UserController.getUsername(user), subjectField.getText(), "A message that you sent to could not be delivered to one or more of its recipients: \n" + toField.getText() + "\n" + messageArea.getText());
-                    Logger.getGlobal().info("Undeliverable message sent");
-                    UserController.addMessage(user, message);
-                    UserController.removeUserFromFile(user);
-                    UserController.writeUserToFile(user);
-                    JOptionPane.getRootFrame().dispose();
-                }
+                Message message=new Message(UserController.getUsername(user), toField.getText(), subjectField.getText(), messageArea.getText());
+                // Add new message to User object.
+                UserController.addMessage(recipient, message);
+                // Delete old User object from file.
+                UserController.removeUserFromFile(recipient);
+                // Write updated User object to file.
+                UserController.writeUserToFile(recipient); // writes updated User object to file.
+                JOptionPane.getRootFrame().dispose();
+            }
+            else{
+                // send generic message could not be delivered message.
+                Message message = new Message(UserController.getUsername(user), UserController.getUsername(user), subjectField.getText(), "A message that you sent could not be delivered to one or more of its recipients: \n" + toField.getText() + "\n" + messageArea.getText());
+                UserController.addMessage(user, message);
+                UserController.removeUserFromFile(user);
+                UserController.writeUserToFile(user);
+                JOptionPane.getRootFrame().dispose();
             }
         });
 
         JButton cancelButton=new JButton("Cancel");
+        cancelButton.addActionListener(event -> JOptionPane.getRootFrame().dispose());
         return new JButton[]{sendButton, cancelButton};
-
     }
 
 
