fd61fca7e0857ee11ce087f29e7e36665c52ff41
Daan Pol <daanpol@tutamail.com>
2019-09-19 15:18:32 +0200

    Added LoginPane

--- src/LoginPane.java
+++ src/LoginPane.java
@@ -5,51 +5,53 @@ import javax.swing.event.DocumentListener;
 import javax.swing.text.*;
 import java.awt.*;
 import java.util.Arrays;
-import java.util.logging.Logger;
 
-public class LoginPane {
+/**
+ * This class provides a login window for the messaging system.
+ */
+class LoginPane{
 
     private JTextField usernameField;
     private JLabel errorLabel;
     private JPasswordField passwordField;
-    private User user;
 
     public LoginPane(){
         JOptionPane.showOptionDialog(null, createMainPanel(), "Login", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, createButtonsArray(), createButtonsArray()[0]);
     }
 
-    class LoginListener implements ActionListener{
+    /**
+     * This class will attempt to login the user. It is added to the loginButton and the passwordField (so the user can login with the enter button instead of the button).
+     */
+    class LoginListener implements ActionListener {
 
-        @Override
-        public void actionPerformed(ActionEvent even){
-
-            user = UserController.getUser(usernameField.getText());
-            char[] password=passwordField.getPassword();
-            if(user!=null){
-                if(Arrays.equals(password, user.getPassword())){
-
+        public void actionPerformed(ActionEvent event) {
+            User user = UserController.getUser(usernameField.getText());
+            char[] password = passwordField.getPassword();
+            if (user != null) {
+                if (Arrays.equals(password, user.getPassword())) {
                     JOptionPane.getRootFrame().dispose();
-
-                    InboxFrame inbox=new InboxFrame(user);
+                    InboxFrame inbox = new InboxFrame(user);
                     inbox.setLocationRelativeTo(null);
                     inbox.setVisible(true);
-                }
-                else{
+                } else {
                     errorLabel.setText("Incorrect password!");
                 }
-            }
-            else{
-
+            } else {
                 errorLabel.setText("Unknown username!");
             }
         }
     }
 
+    /**
+     * This method constructs a JPanel with JLabel and JTextAreas that is passed JOptionPane.showOptionDialog.
+     * @return a JPanel object
+     */
     private JPanel createMainPanel(){
 
         JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
 
-        errorLabel=new ErrorLabel();
+        final int ERROR_LABEL_NR_OF_LINES=1;
+        errorLabel=new ErrorLabel(ERROR_LABEL_NR_OF_LINES);
 
         mainPanel.add(errorLabel, BorderLayout.SOUTH);
         mainPanel.add(createLabelsPanel(), BorderLayout.WEST);
@@ -58,6 +60,10 @@ public class LoginPane {
         return mainPanel;
     }
 
+    /**
+     * This method creates the JLabels for the mainPanel.
+     * @return a JPanel object.
+     */
     private JPanel createLabelsPanel(){
 
         JPanel labelsPanel = new JPanel(new GridLayout(0, 1, 10, 10));
@@ -67,40 +73,60 @@ public class LoginPane {
         return labelsPanel;
     }
 
+    /**
+     * This method creates the JTextFields for the mainPanel.
+     * @return a JPanel object.
+     */
     private JPanel createTextFieldsPanel(){
 
 
+        /**
+         * This class allows for clearing the text of the errorLabel when the user corrects either the username or the password.
+         */
+        class ClearErrorLabelListener implements DocumentListener{
+
+            public void insertUpdate(DocumentEvent documentEvent){
+                clearErrorLabel();
+            }
+
+            public void removeUpdate(DocumentEvent documentEvent) {
+                clearErrorLabel();
+            }
+
+            public void changedUpdate(DocumentEvent documentEvent) {
+            }
+
+            private void clearErrorLabel(){
+                errorLabel.setText("");
+            }
+        }
+
         JPanel textFieldsPanel = new JPanel(new GridLayout(0, 1, 10, 10));
 
-        usernameField = new JTextField();
-        usernameField.addFocusListener(new FocusAdapter(){
 
-            public void focusGained(FocusEvent e){
-                if(user==null){
-                    errorLabel.setText("");
-                }
-            }
-        });
+        usernameField = new JTextField();
+        // add a ClearErrorLabelListener to clear the errorLabel's text when the user corrects the username.
+        usernameField.getDocument().addDocumentListener(new ClearErrorLabelListener());
+
         passwordField = new JPasswordField();
         passwordField.addActionListener(new LoginListener());
-        //passwordField.addFocusListener(new removeErrorTextListener());
 
-        // Password must consist of between 4 and 6 characters containing at least one uppercase and one lowercase character.
-        // I added a DocumentFilter to limit the number of characters in the passwordField to 6.
-
+        // Because the password must consist of 4 to 8 characters(and because I wanted to mess around with DocumentFilter which
+        // was new to me), I added a DocumentFilter to limit the number of characters in the passwordField to 8.
         AbstractDocument document=(AbstractDocument) passwordField.getDocument();
         document.setDocumentFilter(new DocumentFilter(){
 
             @Override
             public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
                 String string=fb.getDocument().getText(0, fb.getDocument().getLength()) + text;
-                if(string.length()<=6){ super.replace(fb, offset, length, text, attrs);
+                if(string.length()<=8){ super.replace(fb, offset, length, text, attrs);
                 }
             }
         });
 
-        // Add a documentListener to clear clear the errorLabel's text when a user corrects the password without the passwordField having lost focus.
-        passwordField.getDocument().addDocumentListener(new DocumentListener() {
+        // Add a documentListener to clear the errorLabel's text when a user corrects the password.
+        passwordField.getDocument().addDocumentListener(new ClearErrorLabelListener());
+       /* passwordField.getDocument().addDocumentListener(new DocumentListener() {
             @Override
             public void insertUpdate(DocumentEvent documentEvent){
                 clearErrorLabel();
@@ -118,7 +144,7 @@ public class LoginPane {
             void clearErrorLabel(){
                     errorLabel.setText("");
                 }
-        });
+        });*/
 
         textFieldsPanel.add(usernameField);
         textFieldsPanel.add(passwordField);
@@ -131,13 +157,9 @@ public class LoginPane {
         JButton loginButton = new JButton("Login");
         loginButton.addActionListener(new LoginListener());
 
+
         JButton registerButton = new JButton("Register");
         registerButton.addActionListener(event -> {
-            /*if(UserController.isFirstWrite()){
-                JOptionPane.showInputDialog(null, "Please provide a path for the user data file: ", null, JOptionPane.INFORMATION_MESSAGE);
-
-
-            }*/
             JOptionPane.getRootFrame().dispose();
             new RegistrationPane();
         });
