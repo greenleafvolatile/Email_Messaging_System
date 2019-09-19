959a61433d83f9d0dd6e257b076bd88c7ac7ae55
Daan Pol <daanpol@tutamail.com>
2019-09-19 15:19:13 +0200

    Added ErrorLabel

--- src/ErrorLabel.java
+++ src/ErrorLabel.java
@@ -6,10 +6,10 @@ import java.util.logging.Logger;
 public class ErrorLabel extends JLabel {
 
 
-    public ErrorLabel() {
+    public ErrorLabel(int nrOfLines) {
         setFont(new Font(this.getFont().getName(), Font.PLAIN, 10));
         setForeground(Color.RED);
-        setPreferredSize(new Dimension(Integer.MIN_VALUE, this.getFont().getSize())); // I'd rather not use this.
+        setPreferredSize(new Dimension(Integer.MIN_VALUE, this.getFont().getSize() * nrOfLines)); // I'd rather not use this.
         setHorizontalAlignment(SwingConstants.CENTER);
         setText("");
     }
