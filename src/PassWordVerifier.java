bf80d48fb714fe483b3664bc974090e520211fb3
Daan Pol <daanpol@tutamail.com>
2019-09-20 15:11:08 +0200

    Added PasswordVerifier

--- src/PassWordVerifier.java
+++ src/PassWordVerifier.java
@@ -1,4 +1,8 @@
-
+/**
+ * This is a utility class with one function: to verify that a password meets a certain standard.
+ * I kept it simple because this project is not about passwords but for me to practice object oriented programming.
+ * Hence, a password has to only consist of 2-4 characters and contain one uppercase and one lowercase character.
+ */
 public class PassWordVerifier {
 
     public static boolean isCorrectPassword(char[] aPassword){
@@ -6,7 +10,7 @@ public class PassWordVerifier {
         String password=new String(aPassword);
         boolean hasUpperCase=!password.equals(password.toLowerCase());
         boolean hasLowerCase=!password.equals(password.toUpperCase());
-        if(hasUpperCase && hasLowerCase && password.length()>=4 && password.length()<=8){
+        if(hasUpperCase && hasLowerCase && password.length()>=2 && password.length()<=4){
             return true;
         }
         return false;
