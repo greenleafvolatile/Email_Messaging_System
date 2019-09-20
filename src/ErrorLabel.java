654e8e4c72bef594467eeb5397210522e8703608
Daan Pol <daanpol@tutamail.com>
2019-09-20 15:08:57 +0200

    Added Error_Label

--- src/ErrorLabel.java
+++ src/ErrorLabel.java
@@ -3,6 +3,10 @@ import java.awt.*;
 
 class ErrorLabel extends JLabel {
 
+    /**
+     * Constructor
+     * @param nrOfLines, the number of lines of text to be displayed on the label.
+     */
     ErrorLabel(int nrOfLines) {
         final int FONT_SIZE=10;
         setFont(new Font(this.getFont().getName(), Font.PLAIN, FONT_SIZE));
