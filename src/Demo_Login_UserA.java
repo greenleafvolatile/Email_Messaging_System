4fc7ff89d195da42988f60b4fbd3731d496041ee
Daan Pol <daanpol@tutamail.com>
2019-09-20 15:01:56 +0200

    Added Demo_Login_UserA

--- src/Demo_Login_UserA.java
+++ src/Demo_Login_UserA.java
@@ -1,6 +1,6 @@
 public class Demo_Login_UserA {
 
-    public static void main(String[] args){
-        LoginPane loginUserA =new LoginPane();
+    public static void main(String[] args) {
+        LoginPane loginUserA = new LoginPane();
     }
 }
